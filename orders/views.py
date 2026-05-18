b8f1d08d5464d5a2ed5f593a948db8678ac00106
Olha <olykagreen220@gmail.com>
2026-05-18 15:05:38 +0300

    Added view for OrderForm

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import OrderForm
 
-# Create your views here.
+def create_order(request):
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            order = form.save(commit=False)
+            order.user = request.user
+            order.save()
+            return redirect('order_success')
+    else:
+        form = OrderForm()
+    return render(request, 'orders/create_order.html', {'form': form})
