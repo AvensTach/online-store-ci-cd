d3fabe2fb0030b91982910cbaa48b71dd16e9a13
Olha <olykagreen220@gmail.com>
2026-05-28 13:29:39 +0300

    implement add_to_cart and cart_detail views

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from shop.models import Product
+from .models import CartItem
 
-# Create your views here.
+def add_to_cart(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    cart_item, created = CartItem.objects.get_or_create(user=request.user, product=product)
+    if not created:
+        cart_item.quantity += 1
+        cart_item.save()
+    return redirect('cart_detail')
+
+def cart_detail(request):
+    items = CartItem.objects.filter(user=request.user)
+    return render(request, 'cart/cart_detail.html', {'items': items})
