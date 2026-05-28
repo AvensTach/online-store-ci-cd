ca84cde2fd95c972ba0e2741d04cbf01a15bb97a
Olha <olykagreen220@gmail.com>
2026-05-28 13:28:32 +0300

    add CartItem model with user, product, and quantity fields

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from shop.models import Product
+from users.models import User
 
-# Create your models here.
+class CartItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.product.title} x {self.quantity}"
