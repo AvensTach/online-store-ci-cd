c9655aedeb21a64f90f424d9e29ccce37c4fada7
Ivan <givan2981@gmail.com>
2026-05-29 12:20:33 +0300

    Tests added

--- cart/tests.py
+++ cart/tests.py
@@ -1,16 +1,17 @@
 from django.test import TestCase
 from django.urls import reverse
 from users.models import User
-from shop.models import Product
+from shop.models import Product, Category
 from .models import CartItem
 
 class CartTest(TestCase):
     def setUp(self):
-        self.user = User.objects.create_user(username='testuser', password='12345')
-        self.product = Product.objects.create(name='Test Product', price=100)
+        self.user = User.objects.create_user(username='testuser', email='test@example.com', password='12345')
+        self.category = Category.objects.create(name='Test Category')
+        self.product = Product.objects.create(name='Test Product', price=100, category=self.category)
         self.client.login(username='testuser', password='12345')
 
     def test_add_to_cart(self):
-        response = self.client.post(reverse('add_to_cart', args=[self.product.id]))
-        self.assertEqual(response.status_code, 302)
+        response = self.client.post(reverse('cart:add_to_cart', args=[self.product.id]), follow=True)
+        self.assertEqual(response.status_code, 200)
         self.assertEqual(CartItem.objects.count(), 1)
